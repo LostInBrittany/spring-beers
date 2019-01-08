57e4ca3bb2f12f2842474feda0fa43b950736c6f
Horacio Gonzalez <horacio.gonzalez@gmail.com>
2019-01-08 04:12:16 +0100

    Initial step-05

---
+++ step-05/src/main/java/org/lostinbrittany/cesi/springbeers/controllers/BeerController.java
@@ -0,0 +1,38 @@
+package org.lostinbrittany.cesi.springbeers.controllers;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.List;
+
+import org.lostinbrittany.cesi.springbeers.model.Beer;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RestController
+public class BeerController {
+
+	@RequestMapping("/beers")
+	public List<Beer> getBeers() {
+		return Beer.getBeers();
+	}
+
+	@RequestMapping(value = "/beer/{id}", method = RequestMethod.GET)
+	public Beer beerDetails(@PathVariable("id") String id) {
+
+		ObjectMapper mapper = new ObjectMapper();
+		
+		try {
+			File file = ResourceUtils.getFile("classpath:beers/" + id + ".json");
+			FileInputStream in = new FileInputStream(file);
+			Beer beer = mapper.readValue(file, Beer.class);
+			return beer;
+		} catch (Exception e) {
+			return null;
+		}
+	}
+}
