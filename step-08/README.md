1fb4779ad7ba94b167fdc8c5a967e784dec42bca
Horacio Gonzalez <horacio.gonzalez@gmail.com>
2019-01-08 16:14:15 +0100

    Security

--- step-08/README.md
+++ step-08/README.md
@@ -20,3 +20,53 @@ dependencies {
 }
 ```
 
+## Adding security configuration
+
+Now we are going to add a security configuration, `WebSecurityConfig.java` that ensures that only authenticated users can access the protected endpoints.
+
+```java
+package org.lostinbrittany.cesi.springbeers;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            	.antMatchers("/create", "/delete/", "/delete/*", "/edit").authenticated()
+                .antMatchers("/", "/**/*.*", "/beer/list", "/beer/details").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            	.csrf().disable();
+    }
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+             User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
+```
+
+The `WebSecurityConfig` class is annotated with `@EnableWebSecurity` to enable Spring Security’s web security support and provide the Spring MVC integration. It also extends `WebSecurityConfigurerAdapter` and overrides a couple of its methods to set some specifics of the web security configuration.
+
+The `configure(HttpSecurity)` method defines which URL paths should be secured and which should not. 
+
+As for the `userDetailsService()` method, it sets up an in-memory user store with a single user. That user is given a username of "user", a password of "password", and a role of "USER".
--- step-08/src/main/java/org/lostinbrittany/cesi/springbeers/WebSecurityConfig.java
+++ step-08/src/main/java/org/lostinbrittany/cesi/springbeers/WebSecurityConfig.java
@@ -1,42 +1,38 @@
 package org.lostinbrittany.cesi.springbeers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-@EnableWebMvcSecurity
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-        	.httpBasic().and()
             .authorizeRequests()
+            	.antMatchers("/create", "/delete/", "/delete/*", "/edit").authenticated()
                 .antMatchers("/", "/**/*.*", "/beer/list", "/beer/details").permitAll()
                 .anyRequest().authenticated()
                 .and()
-             /*   
-            .formLogin()
-                .loginPage("/index.html#/login")
-                .defaultSuccessUrl("/index.html", true)
-                .failureUrl("/partials/loginFail.html")
-                .loginProcessingUrl("/login")
-                .permitAll()
-                .and()
-                */
-            .logout()
-                .permitAll()
-            .and()
             	.csrf().disable();
     }
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+             User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
 
-/*    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
-    }*/
+        return new InMemoryUserDetailsManager(user);
+    }
 }
